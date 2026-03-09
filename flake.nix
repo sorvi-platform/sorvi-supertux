35a4c71dd55a3eeb043574422480199ff2170824
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2026-03-09 19:10:17 +0900

    update to 0.15.2

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       # Zig flake helper
       # Check the flake.nix in zig2nix project for more options:
       # <https://github.com/Cloudef/zig2nix/blob/master/flake.nix>
-      env = zig2nix.outputs.zig-env.${system} { zig = zig2nix.outputs.packages.${system}.zig-0_15_1; };
+      env = zig2nix.outputs.zig-env.${system} { zig = zig2nix.outputs.packages.${system}.zig-0_15_2; };
 
       frontend_runtime_deps = with env.pkgs; [
         vulkan-loader
